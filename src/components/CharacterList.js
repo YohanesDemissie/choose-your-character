1e3f59e51e4b81945554a388baae7b4972a34134
Yohanes Demissie <Yohanesdemissie@gmail.com>
2020-07-08 14:49:08 -0700

    selected heroes rendering onto DOM successfully

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { bindActionCreators } from 'redux';
+// import { bindActionCreators } from 'redux';
 import { addCharacterById } from '../actions'
 
 class CharacterList extends Component {
   render() {
     console.log(this.props, 'HE;OOOOO')
+    console.log(this.state, 'STATEEEEEE')
     return(
       <div>
         <h4>Heros</h4>
@@ -18,7 +19,18 @@ class CharacterList extends Component {
                   <div>{character.name}</div>
                   <div
                     onClick={() => this.props.addCharacterById(character.id)}> + </div>
+                    <div>
+                    </div>
                 </li>
+              )
+            })
+          }
+        </ul>
+        <ul>
+          {
+            this.props.heroes.map(hero => {
+              return(
+                <li key={hero.id}>{hero.name}</li>
               )
             })
           }
@@ -29,14 +41,15 @@ class CharacterList extends Component {
 }
 
 function mapStateToProps(state) {
-  console.log('state', state);
+  console.log('state', state.heroes);
   return {
-    characters: state.characters
+    characters: state.characters,
+    heroes: state.heroes
   };
 }
 
-function mapDispatchToProps(dispatch) { // look up mapStateToprops
-  return bindActionCreators({ addCharacterById }, dispatch) // look up mapdispatch on redux
-}
+// function mapDispatchToProps(dispatch) { // look up mapStateToprops
+//   return bindActionCreators({ addCharacterById }, dispatch) // look up mapdispatch on redux
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CharacterList);
+export default connect(mapStateToProps, {addCharacterById})(CharacterList);
