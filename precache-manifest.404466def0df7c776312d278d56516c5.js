6d693ec24e3667b79169d4887b748671e75b5b8c
Yohanes Demissie <Yohanesdemissie@gmail.com>
2024-11-06 18:30:22 -0800

    Updates

---
+++ precache-manifest.404466def0df7c776312d278d56516c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f458b57d4a3b1f6087d75421a3b799cb",
+    "url": "/choose-your-character/index.html"
+  },
+  {
+    "revision": "f7b066c4514a63a8408b",
+    "url": "/choose-your-character/static/css/main.2d6e5d94.chunk.css"
+  },
+  {
+    "revision": "71131ca09f2d5e2a1fa6",
+    "url": "/choose-your-character/static/js/2.238ad038.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/choose-your-character/static/js/2.238ad038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b066c4514a63a8408b",
+    "url": "/choose-your-character/static/js/main.afa82634.chunk.js"
+  },
+  {
+    "revision": "5c3e1e91f791737cc0bc",
+    "url": "/choose-your-character/static/js/runtime-main.6365e397.js"
+  },
+  {
+    "revision": "4ddc758d490102bf391845e6f0791725",
+    "url": "/choose-your-character/static/media/background.4ddc758d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0039a62485271c1e3629b0f191df3a4e.js"
+  "/choose-your-character/precache-manifest.404466def0df7c776312d278d56516c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/choose-your-character/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
